4c40e8461d3c527e5ad16a45d792df75987e661a
FruitKount <66969646+agronomofiorentini@users.noreply.github.com>
2026-01-14 10:45:46 +0100

    Add files via upload

---
+++ R/get_token.R
@@ -0,0 +1,39 @@
+library(httr)
+library(tidyverse)
+library(jsonlite)
+
+get_token<-function(email,
+                    password) {
+
+  # Set the domain
+  domain <- "https://backend.fruitkount.com/"
+
+  # Set the endpoint
+  endpoint <- "authentication/token/"
+
+  # Create the API URL
+  api_url <- paste0(domain, endpoint)
+
+  # Create the JSON body with email and password
+  json_body <- list(email = email,
+                    password = password)
+
+  # Make the POST request
+  response <- POST(
+    api_url,
+    body = json_body,
+    encode = "json", # Ensures the body is sent as JSON
+    content_type_json() # Sets the Content-Type header to application/json
+  )
+
+  # Check the status code of the response
+  print(response$status_code)
+
+  # Check the content
+  cont <- content(response, as = "text", type = "application/json", encoding="UTF-8")
+  cont<-fromJSON(cont) %>% as.data.frame
+
+  # The following is the Bearer code that you have to use for each request
+  # print(cont$token)
+}
+
---
+++ README.md
@@ -0,0 +1,22 @@
+# FruitKountRPackage
+
+The goal of FruitKountRPackage is to ...
+
+## Installation
+
+You can install the development version of FruitKountRPackage like so:
+
+``` r
+devtools::install_github("https://github.com/agronomofiorentini/fruitkount-r-package")
+```
+
+## Authentication
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(FruitKountRPackage)
+get_token(email,
+          password)
+```
+
